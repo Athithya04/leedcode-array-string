56b58d008735dfc08932690014f841b84c54d099
Athithya04 <38552575+Athithya04@users.noreply.github.com>
2024-02-02 14:23:27 +0530

    Update Final Value of Variable After Performing Operations.c

--- Final Value of Variable After Performing Operations.c
+++ Final Value of Variable After Performing Operations.c
@@ -1,1 +1,9 @@
-
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+int *a=malloc(sizeof(int)*numsSize);
+*returnSize= numsSize;
+    for(int  i=0;i<numsSize;i++)
+    {
+        a[i]=nums[nums[i]];
+    }
+    return a;
+}
