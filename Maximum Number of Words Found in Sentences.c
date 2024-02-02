0f75269e76bb344b888925fddf5bf41bdc8f4702
Athithya04 <38552575+Athithya04@users.noreply.github.com>
2024-02-02 20:10:51 +0530

    Update Maximum Number of Words Found in Sentences.c

--- Maximum Number of Words Found in Sentences.c
+++ Maximum Number of Words Found in Sentences.c
@@ -1,12 +1,19 @@
 int mostWordsFound(char** sentences, int sentencesSize) {
-int i,j,c=1,max=0;
+int i,j,c=0,max=0;
     for(i=0;i<sentencesSize;i++)
     {
-        for(j=0,c=1;sentences[i][j];j++)
+        for(j=0,c=0;sentences[i][j];j++)
+        {
        if(sentences[i][j]==' ')
-       c++;
+        {
+            c++;
         }
+    }
+    
         if(c>max)
-        max=c;
-        return max;
+        {
+            max=c;
+    }
+    }
+        return max+1;
 }
